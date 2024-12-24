9219a24d768294554437c61544a26d980fc749c3
Aaditya Thakare <aadityathakare124@gmail.com>
2024-12-25 01:31:59 +0530

    Home Page Products Fetched and Rendered!

--- Frontend/src/Components/Card.jsx
+++ Frontend/src/Components/Card.jsx
@@ -1,18 +1,16 @@
 import React from 'react';
 import './Components.css';
 
-const Card = () => {
+const Card = (props) => {
   return (
-
-
     <div className="card">
      <img 
        src="\src\assets\images\card1.png" // Replace with your image URL
-        alt="Dairy Milk Oreo" 
+        alt={props.name}
         className="product-image"
       />
-      <h3 className="product-title">Dairy Milk Oreo</h3>
-      <div className="discount-badge">15% <span style={{fontSize:"18px"}}>Off</span></div>
+      <h3 className="product-title">{props.name}</h3>
+      <div className="discount-badge">{props.offer}% <span style={{fontSize:"18px"}}>Off</span></div>
     </div>
 
   );
